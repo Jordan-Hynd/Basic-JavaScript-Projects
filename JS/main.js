2f5287dc016d8b8b811edfa735552441cf8a1931
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-02-20 13:35:45 -0800

    finished project 1
    created str var and button

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function () { //creating a function
+    var str = "This text is green.", Red = "This text is red." ; //creating a str var
+    var result = str.fontcolor("green"); //coloring the srt
+    document.getElementById("Green_Text").innerHTML = result; //id the function
+}
