619cc7c5bc75ec2a6394e6a94aff4a1249d55f1a
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-02-20 14:18:13 -0800

    function2
    created a p function

---
+++ JS/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Function2 () { //creating a function
+    var sentece = "I\'m learning a lot" ; //creating a str var
+    sentece += " from this program";
+    document.getElementById("Color_Text").innerHTML = sentece; //id the function
+}
