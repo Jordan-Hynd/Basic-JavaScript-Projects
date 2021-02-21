4d025180c5819c2b9b3a5b863f8ce8dd0983d255
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-02-21 08:05:40 -0800

    finished
    completed math functions

--- JS/Project3_math_operators/JS/main.js
+++ JS/Project3_math_operators/JS/main.js
@@ -1,6 +1,40 @@
-function Subtraction_Function () {
-    var Subtraction = 5 - 2; 
-    document.getElementById("Math").innerHTML = "5 - 2  = " + Subtraction;
+function Subtraction_Function () { //naming the function
+    var Subtraction = 5 - 2; //creating the var
+    document.getElementById("Math-").innerHTML = "5 - 2  = " + Subtraction; //id the function
 }
 
-alert("hello");
+function Add_Function () { //naming the function
+    var Addition = 5 + 2; //creating the var
+    document.getElementById("Math+").innerHTML = "5 + 2  = " + Addition; //id the function
+}
+
+function Divide_Function () { //naming the function
+    var Division = 5 / 2; //creating the var
+    document.getElementById("Math/").innerHTML = "5 / 2  = " + Division; //id the function
+}
+
+function Multiply_Function () { //naming the function
+    var Multiplication = 5 * 2; //creating the var
+    document.getElementById("Math*").innerHTML = "5 * 2  = " + Multiplication; //id the function
+}
+
+function Modulus_Function () { //naming the function
+    var Modulus = 25 % 6; //creating the var
+    document.getElementById("Math%").innerHTML = "25 % 6  = " + Modulus; //id the function
+}
+
+function Random_Number() { //naming the function
+    document.getElementById("Random").innerHTML = "Your number is " + Math.random(); //id the function
+  }
+
+  function Plus1() { //naming the function
+    var a = 5; //creating the var
+    var b = a++; //creating the var
+    document.getElementById("Math++").innerHTML = b; //id the function
+  }
+
+  function Minus1() { //naming the function
+    var c = 6; //creating the var
+    var d = --c; //creating the var
+    document.getElementById("Math--").innerHTML = d; //id the function
+  }
