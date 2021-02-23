6360ab12655b0a41a4b58d21565bbeca8103aba9
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-02-23 15:22:50 -0800

    finished pj5
    finished

---
+++ JS/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,38 @@
+function my_Function () { //creating the function
+    document.getElementById("Test").innerHTML = 0/0; //id the function and call method
+}
+
+function my_Function2 () { //creating and naming function
+    document.getElementById("Test2").innerHTML = isNaN("This is a string"); //id and call method
+}
+
+function my_Function3 () { //creating and naming function
+    document.getElementById("Test3").innerHTML = isNaN("007"); //id and call method
+}
+
+function my_Function4 () { //creating and naming the function
+    document.getElementById("Test4").innerHTML = 1.797631332432134513412E+10308 + "<br>" + -1.797631332432134513412E+10308 ; //id and call method
+}
+
+function Function1 () { //creating and naming the fuction
+    document.write(10 == 10); //call method
+    
+}
+
+function Function2 () { //creating and naming the function
+    x = 100; //define var
+    y = 100; //define var
+    document.write(x === y); //call method and operator
+}
+
+function logic_function () { //creating and namming the fucntion
+    document.write(5 > 2 &&  10 > 4); //call method and operator
+}
+
+function logic_function2 () { //creating and naming the function 
+    document.write(5 > 10 ||  10 > 4); //call method and operator 
+}
+
+function logic_function3 () { //creating and naming function
+    document.write(!(5 > 2 && 10 > 4)); //call method and operator
+}
