c428f76017c6da9ebd633576093c5e908e0b92ea
Jordan-Hynd <78391991+Jordan-Hynd@users.noreply.github.com>
2021-02-27 17:12:45 -0800

    project10
    finished project 10

--- JS/Project10_/JS/main.js
+++ JS/Project10_/JS/main.js
@@ -1,19 +1,20 @@
+//call loop function
 function Call_Loop() {
     var num = "";
-    var X = 10;
+    let X = 10;
     while (X > 0) {
         num += "<br>" + X;
         X--;
     }
     document.getElementById("Loop").innerHTML = num;
 }
-
+//string function
 function String_Function() {
     var str = "This is my function.";
     var n = str.length;
     document.getElementById("strf").innerHTML = n;
   }
-
+//loop function
   var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
   var Content = "";
   var Y;
@@ -23,7 +24,7 @@ function String_Function() {
       }
       document.getElementById("List_of_Instruments").innerHTML = Content;
   }
-
+//function for array
   function array_function() {
       var schedule = [];
       schedule[0] = "wake up";
@@ -33,13 +34,56 @@ function String_Function() {
       document.getElementById("array").innerHTML = "it is time to " + schedule[0] + ".";
   }
 
-
-
+//function for const
   function constant_function() {
       const musical_instrument = {type: "guitar", brand:"fender", color:"black"};
       musical_instrument.color = "blue";
       musical_instrument.price = "$900";
+      musical_instrument.type = "guitar";
       document.getElementById("constant").innerHTML = "The cost of the " + musical_instrument.type + " was " + musical_instrument.price;
   }
-  constant_function["musical_instrument.wh"] = "heavy";
-  constant_function["musical_instrument.price"] = "$800";
+//function for let
+  function let_f() {
+      let X = 100;
+      document.getElementById("let_f").innerHTML = "x is equal to " + X;
+  }
+//return statement
+var X = math_function(500, 624);
+
+function math_function(A, B) {
+    return A * B
+}
+document.getElementById("math").innerHTML = X;
+
+//object data structure 
+let feb_27 = {
+    temp: "cold ",
+    light: "dark ",
+    wind: "windy ",
+    description: function () {
+        return "Today was a "  + this.temp + "and " + this.wind + "day";
+    }
+};
+document.getElementById("today").innerHTML = feb_27.description();
+
+//break loop
+function break_loop() {
+    var text = "";
+    var A;
+    for (A = 0; A < 8; A++) {
+      if (A === 4) { break; }
+      text += "The number is " + A + "<br>";
+    }
+document.getElementById("break").innerHTML = text;
+}
+
+//
+function cont_loop() {
+    var text = "";
+    var A;
+    for (A = 0; A < 8; A++) {
+      if (A === 4) { continue; }
+      text += "The number is " + A + "<br>";
+    }
+document.getElementById("cont").innerHTML = text;
+}
